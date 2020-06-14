003097ddf9c77630267a2664423f88770c996902
tomas <ahoj@tomaskudlac.sk>
2020-06-14 22:04:49 +0200

    Add loading of CSS, move CSS from style tag to main Stylus file

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,12 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mnemosaurus</title>
-    <style>
-        html,
-        body {
-            background-color: #222;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
     <script src="{{asset('js/app.js')}}" defer></script>
 </head>
 

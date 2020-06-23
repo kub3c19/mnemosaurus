2615acefbaefbddb76e9ee6801442abfdd82295a
Jakub Kudlac <kudlac@visibility.sk>
2020-06-23 13:39:27 +0200

    Fix down function in make username unique migration

--- database/migrations/2020_06_15_073734_make_username_unique.php
+++ database/migrations/2020_06_15_073734_make_username_unique.php
@@ -26,7 +26,7 @@ class MakeUsernameUnique extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropUnique(['username', 'email']);
+            $table->unique('email');
         });
     }
 }

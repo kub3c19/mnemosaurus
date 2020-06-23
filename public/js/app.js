5fbdb89eef583185f222cbdb293c6c66699bad7a
tomas <ahoj@tomaskudlac.sk>
2020-06-23 20:19:40 +0200

    Sync generated CSS and JS

--- public/js/app.js
+++ public/js/app.js
@@ -1627,6 +1627,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -1664,16 +1673,7 @@ let AddMnemonic = class AddMnemonic extends vue_class_component_1.mixins(Form_1.
                 error: ``,
                 label: `Jazyk 1`,
                 name: `language1`,
-                options: [
-                    {
-                        label: `Angličtina`,
-                        value: `en`
-                    },
-                    {
-                        label: `Slovenčina`,
-                        value: `sk`
-                    }
-                ],
+                options: [],
                 order: 0,
                 tag: `select`,
                 type: ``,
@@ -1683,16 +1683,7 @@ let AddMnemonic = class AddMnemonic extends vue_class_component_1.mixins(Form_1.
                 error: ``,
                 label: `Jazyk 2`,
                 name: `language2`,
-                options: [
-                    {
-                        label: `Angličtina`,
-                        value: `en`
-                    },
-                    {
-                        label: `Slovenčina`,
-                        value: `sk`
-                    }
-                ],
+                options: [],
                 order: 2,
                 tag: `select`,
                 type: ``,
@@ -1708,6 +1699,29 @@ let AddMnemonic = class AddMnemonic extends vue_class_component_1.mixins(Form_1.
                 value: ``
             }
         };
+    }
+    languagesGet() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return fetch(`/languages`).then((response) => __awaiter(this, void 0, void 0, function* () {
+                if (response.status >= 400) {
+                    this.$root.$emit(`alert-show`, {
+                        message: `Unable to load languages`,
+                        type: `failed`
+                    });
+                    return;
+                }
+                return (yield response.json()).map(({ id, name }) => {
+                    return { label: name, value: id.toString() };
+                });
+            })).catch(console.error);
+        });
+    }
+    created() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const languages = (yield this.languagesGet()) || [];
+            this.fields.language1.options = languages;
+            this.fields.language2.options = languages;
+        });
     }
 };
 AddMnemonic = __decorate([
@@ -2553,7 +2567,12 @@ var render = function() {
                               key: option.value,
                               domProps: { value: option.value }
                             },
-                            [_vm._v(_vm._s(option.label))]
+                            [
+                              _vm._v(
+                                _vm._s(option.label) +
+                                  "\n                        "
+                              )
+                            ]
                           )
                         }),
                         0
